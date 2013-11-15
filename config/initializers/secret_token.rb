9f2beab301e002dbfa458886ea5f988dc7e22dfe
islador <luke.isla@gmail.com>
2013-11-15 10:17:24 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PilotTravel::Application.config.secret_token = '02e32a1ed7a25cc1ff5e5b7132743165c9da403f0799845259fb00b72ae0a47aaeac90631b73909100cd2e7ddb8eea5b0bb8233b26586b12176938e3ccd2e8f2'
