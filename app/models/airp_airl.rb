bbc962795c3a33b12815b33b61f3bd665bfd237f
islador <luke.isla@gmail.com>
2013-11-15 17:10:00 -0800

    Renamed airp_aril.rb to airport_airline.rb

--- app/models/airp_airl.rb
+++
@@ -1,9 +0,0 @@
-class AirpAirl < ActiveRecord::Base
-  attr_accessible :airline_id, :airport_id
-
-  belongs_to :airline, class_name: "Airline"
-  belongs_to :airport, class_name: "Airport"
-
-  validates :airline_id, presence: true
-  validates :airport_id, presence: true
-end
