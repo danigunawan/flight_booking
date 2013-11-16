679395349993b4bc2908dde2857a1cc35c91248f
islador <luke.isla@gmail.com>
2013-11-15 18:41:07 -0800

    Built up the flight and plane models along with their shared association. Flights is not yet associated with Airlines.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,15 @@
 class Flight < ActiveRecord::Base
-  attr_accessible :airline_id, :arrival, :bus_avail, :bus_fare, :date, :departure, :destination_airport, :eco_avail, :eco_fare, :number, :origin_airport, :plane_id
+  attr_accessible :airline_id, :arrival, :bus_avail, :bus_fare, :date, :departure, :destination_airport, :eco_avail, :eco_fare, :number, :origin_airport
+
+  has_one :plane
+
+  validates :airline_id, presence: true
+  validates :arrival, presence: true
+  validates :bus_fare, presence: true
+  validates :eco_fare, presence: true
+  validates :date, presence: true
+  validates :departure, presence: true
+  validates :destination_airport, presence: true
+  validates :number, presence: true
+  validates :origin_airport, presence: true
 end
--- app/models/plane.rb
+++ app/models/plane.rb
@@ -1,3 +1,13 @@
 class Plane < ActiveRecord::Base
-  attr_accessible :bus_cap, :eco_cap, :manufacturer, :prop_type, :tail_num
+  attr_accessible :bus_cap, :eco_cap, :manufacturer, :prop_type, :tail_num, :flight_id, :type
+
+  belongs_to :flight
+
+  validates :bus_cap, presence: true
+  validates :eco_cap, presence: true
+  validates :manufacturer, presence: true
+  validates :type, presence: true
+  validates :prop_type, presence: true
+  validates :tail_num, presence: true
+  validates :flight_id, presence: true
 end
