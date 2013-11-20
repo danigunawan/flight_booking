ade7a2d83f1a56fde12633e30ae9da2c10751b19
islador <luke.isla@gmail.com>
2013-11-19 19:33:17 -0800

    Updated seeds.rb to seed the database with airports from /db/seed/airports.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+airports = YAML.load_file("#{Rails.root}/db/seed/airports.yml")
+puts "Begining to load airports."
+airports.each do |port|
+	# sta contains a hash of symbols, not a hash of strings/ints
+	yaml_station = Airport.new(name: port[:name], country: port[:country], i_code: port[:i_code], city: port[:city])
+
+	yaml_station[:phone] = rand(1000000000..9999999999)
+	yaml_station.save
+	puts "Airports are %" + (airports.index(port) / 9).to_s + " finished."
+end
+puts "Airports Loaded successfully"
