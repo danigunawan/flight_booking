d9ff3b0ac5bbd9a94e339bcdb888faa8457a5556
islador <luke.isla@gmail.com>
2013-12-14 00:30:26 -0800

    Explored pagination with will_paginate commands on _flight_table and flights/index. Began writing specs for the flight controller filter method, filter behavior needs to be explored; should the filter drop downs be rewritten each time to narrow the searches, or left static from the first search? What does market_monitor do and how does that apply here?

--- spec/controllers/flights_controller_spec.rb
+++ spec/controllers/flights_controller_spec.rb
@@ -34,4 +34,19 @@ describe FlightsController do
 			expect(assigns(:display_prices)).to eq(price)
 		end
 	end
+
+	describe "filter" do
+		before do
+			airport1 = Airport.create(city: "San Francisco", country: "United States of America", i_code: "SFO", name: "San Francisco International Airport", phone: 6508218211)
+			airport2 = Airport.create(city: "Los Angeles", country: "United States of America", i_code: "LAX", name: "Los Angeles International Airport", phone: 6508218222)
+			airline = Airline.create(name: "Virgin America", phone: 5551234567)
+			flight = airline.flights.create(airline_id: airline.id, arrival: DateTime.now+(5/24.0), bus_fare: 500, eco_fare: 250, date: Date.today, departure: DateTime.now+(1/24.0), destination_airport: airport2.id, number: 202, origin_airport: airport1.id)
+			flight.create_plane(bus_cap: 40, eco_cap: 122, manufacturer: "Boeing", make: "737-800", prop_type: "Jet", tail_num: 4285)
+			airport3 = Airport.create(city: "San Jose", country: "United States of America", i_code: "SJC", name: "San Jose International Airport", phone: 6508218211)
+			airport4 = Airport.create(city: "Los Vegas", country: "United States of America", i_code: "LAS", name: "Los Vegas International Airport", phone: 6508218222)
+			airline2 = Airline.create(name: "United Airlines", phone: 5551234567)
+			flight2 = airline.flights.create(airline_id: airline.id, arrival: DateTime.now+(5/24.0), bus_fare: 550, eco_fare: 300, date: Date.today, departure: DateTime.now+(1/24.0), destination_airport: airport2.id, number: 202, origin_airport: airport1.id)
+			flight2.create_plane(bus_cap: 40, eco_cap: 122, manufacturer: "Boeing", make: "737-800", prop_type: "Jet", tail_num: 4285)
+		end
+	end
 end
