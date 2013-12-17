0ac3982212a479d3cc2041728ed08948d7b344bd
islador <luke.isla@gmail.com>
2013-12-17 13:53:34 -0800

    Began writing basic tests for select box behavior and adapted some of the jquery code from market monitor into this project. Realized factory girl is necessary for proper testing of the select functionality.

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -12,3 +12,49 @@ $(function (){
 	   });
 });
 
+$(document).ready(function(){
+	//On change, trigger this function
+	$("#airline_select, #origin_select, #dest_select, #price_select, #select_date, #min_seats").change(function(){
+		//Load all select's values into variables
+		var airline = $("#airline_select").val();
+		var origin = $("#origin_select").val();
+		var dest = $("#dest_select").val();
+		var price = $("#price_select").val();
+		var date = $("#select_date").val();
+		var seat = $("#min_seats").val();
+		//Fire an AJAX call to flights/filter
+	    $.ajax({
+	    	url: "flights/filter", type: "GET",
+	    	//Pass in each variable as a parameter.
+	    	data: { airline_id: airline, 
+	    		origin_airport_id: origin, 
+	    		dest_airport_id: dest, 
+	    		price: price,
+	    		departure_date: date,
+	    		min_seat_count: seat }
+	    });
+	});
+
+	function hider() {
+		//Hide or Show table divs based on whether the table's tbody has content.
+		if ($.trim($("#corp_tbody").html())=='') {
+			$("#corporationtable").hide()
+		}
+		if ($.trim($("#corp_tbody").html())!='') {
+			$("#corporationtable").show()
+		}
+		if ($.trim($("#char_tbody").html())=='') {
+			$("#charactertable").hide()
+		}
+		if ($.trim($("#char_tbody").html())!='') {
+			$("#charactertable").show()
+		}
+	}
+});
+
+
+
+
+
+
+
--- spec/features/flight_view_spec.rb
+++ spec/features/flight_view_spec.rb
@@ -10,8 +10,8 @@ describe "FlightPage" do
 
 		airport1 = Airport.create(city: "San Francisco", country: "United States of America", i_code: "SFO", name: "San Francisco International Airport", phone: 6508218211)
 		airport2 = Airport.create(city: "Los Angeles", country: "United States of America", i_code: "LAX", name: "Los Angeles International Airport", phone: 6508218222)
-		airline = Airline.create(name: "Virgin America", phone: 5551234567)
-		@flight = airline.flights.create(airline_id: airline.id, arrival: DateTime.now+(5/24.0), bus_fare: 500, eco_fare: 250, date: Date.today, departure: DateTime.now+(1/24.0), destination_airport: airport2.id, number: 202, origin_airport: airport1.id)
+		@airline = Airline.create(name: "Virgin America", phone: 5551234567)
+		@flight = @airline.flights.create(airline_id: @airline.id, arrival: DateTime.now+(5/24.0), bus_fare: 500, eco_fare: 250, date: Date.today, departure: DateTime.now+(1/24.0), destination_airport: airport2.id, number: 202, origin_airport: airport1.id)
 		@flight.create_plane(bus_cap: 40, eco_cap: 122, manufacturer: "Boeing", make: "737-800", prop_type: "Jet", tail_num: 4285)
 			visit root_path	
 	end
@@ -44,6 +44,16 @@ describe "FlightPage" do
 
 		it "should contain a select with id 'min_seats'" do
 			should have_selector('select#min_seats')
+		end
+	end
+
+	describe "airline_select" do
+		it "should contain a default option for 'all airlines'" do
+			page.has_select?('airline_select', :with_options => ['All Airlines'])
+		end
+
+		it "should contain an option for each airline" do
+			page.has_select?('airline_select', :with_options => [@airline.name])
 		end
 	end
 
