3561220cc31c0aa35a01e5e3bcf38ad5ee459fb2
islador <luke.isla@gmail.com>
2013-12-30 14:06:45 -0800

    Broke out the code to build the selects into the flights_contoller#build_selects private method to DRY up the code and follow SRP more strictly.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,65 +1,28 @@
 class FlightsController < ApplicationController
 	def show
-		id = []
-		name = []
-		orig_id = []
-		orig_name = []
-		dest_id = []
-		dest_name = []
-		fares = []
-		departure_dt = []
-		arrival_dt = []
 		@flights = Flight.paginate(:page => params[:page]).where("eco_avail > 0 OR bus_avail > 0")
 
-		@flights.each do |f|
-		#Flight.limit(30).each do |f|
-			airline = f.airline
-			id.include?(airline.id) ? nil : id.push(f.airline.id)
-			name.include?(airline.name) ? nil : name.push(airline.name)
-			orig_id.include?(f.origin_airport) ? nil : orig_id.push(f.origin_airport)
-			dest_id.include?(f.destination_airport) ? nil : dest_id.push(f.destination_airport)
-			fares.include?(f.eco_fare) ? nil : fares.push(f.eco_fare)
-			fares.include?(f.bus_fare) ? nil : fares.push(f.bus_fare)
-			departure_dt.include?(f.departure) ? nil : departure_dt.push(f.departure)
-			arrival_dt.include?(f.arrival) ? nil : arrival_dt.push(f.arrival)
-		end
-		@airlines = {"name" => name, "id" => id}
+		extract = build_selects(@flights)
 
-		dest_id.each do |di|
-			dest_name.push(Airport.where("id = ?", di)[0].name)
-		end
-		@destinations = {"name" => dest_name, "id" => dest_id}
+		@airlines = extract[:airlines]
 
-		orig_id.each do |oi|
-			orig_name.push(Airport.where("id = ?", oi)[0].name)
-		end
-		@origins = {"name" => orig_name, "id" => orig_id}
+		@destinations = extract[:destinations]
 
-		@display_prices = calc_display_prices(fares).sort
+		@origins = extract[:origins]
 
-		#@departure_date = []
-		#departure_dt.each do |dt|
-		#	dd = dt.to_date
-		#	unless @departure_date.include?(dd)
-		#		@departure_date.push(dt)
-		#	end
-		#end
+		@display_prices = extract[:display_prices]
 
 		#use a jquery date picker for date selection.
 		#http://jqueryui.com/datepicker/
 		#@dater = @departure_date[0]
 		#date = Date.parse(@date)
-
-
-		#@airlines = {"name" => Flight.limit(30).each {|f| f.airline.name}, "id" => Flight.limit(30).each {|f| f.airline.id}}
-		#@airlines = {"name" => airlines.pluck("name"), "id" => Airline.pluck("id")}
 		render 'index'
 	end
 
 	private
 
 	def calc_display_prices(fares)
-		#Take a fares object and output an array of unique fare prices rounded to the next highest hundred.
+		#Takes a fares object and outputs a sorted array of unique fare prices rounded to the next highest hundred.
 		#eg, a fare of 250 becomes 300, and a fare of 225 becomes 300.
 		display_prices = []
 		fares.each do |f|
@@ -69,7 +32,44 @@ class FlightsController < ApplicationController
 			end
 		end
 
-		return display_prices
+		return display_prices.sort
 	end
 
+	def build_selects(flights)
+		#Takes a flights object, and extracts out all unique destinations, origins and airlines.
+		#Invokes calc_display_prices to produce a fare list.
+
+		id = []
+		name = []
+		orig_id = []
+		orig_name = []
+		dest_id = []
+		dest_name = []
+		fares = []
+		departure_dt = []
+		arrival_dt = []
+
+		flights.each do |f|
+			airline = f.airline
+			id.include?(airline.id) ? nil : id.push(f.airline.id)
+			name.include?(airline.name) ? nil : name.push(airline.name)
+			orig_id.include?(f.origin_airport) ? nil : orig_id.push(f.origin_airport)
+			dest_id.include?(f.destination_airport) ? nil : dest_id.push(f.destination_airport)
+			fares.include?(f.eco_fare) ? nil : fares.push(f.eco_fare)
+			fares.include?(f.bus_fare) ? nil : fares.push(f.bus_fare)
+		end
+		airlines = {"name" => name, "id" => id}
+
+		dest_id.each do |di|
+			dest_name.push(Airport.where("id = ?", di)[0].name)
+		end
+		destinations = {"name" => dest_name, "id" => dest_id}
+
+		orig_id.each do |oi|
+			orig_name.push(Airport.where("id = ?", oi)[0].name)
+		end
+		origins = {"name" => orig_name, "id" => orig_id}
+
+		return {:airlines => airlines, :destinations => destinations, :origins => origins, :display_prices => calc_display_prices(fares)}
+	end
 end
